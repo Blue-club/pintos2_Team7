6a83ef72211d8e7debc1a61dc4355ae63eed50b0
inhokang <ubuntu@ip-172-31-42-48.ap-northeast-2.compute.internal>
2023-06-21 13:25:18 +0000

    stp_insert_page수정

--- vm/vm.c
+++ vm/vm.c
@@ -111,19 +111,11 @@ struct page * spt_find_page (struct supplemental_page_table *spt UNUSED, void *v
 /* Insert PAGE into spt with validation. */
 /*깃북 : 주어진 supplemental page table에 struct page를 삽입하시오
 이 함수는 가상 주소가 주어진 supplemental page table에 존재하지 않는지 확인해야 합니다*/
-bool spt_insert_page (struct supplemental_page_table *spt UNUSED, struct page *page UNUSED) 
+bool spt_insert_page(struct supplemental_page_table *spt UNUSED,
+					 struct page *page UNUSED)
 {
-	int succ = false;
 	/* TODO: Fill this function. */
-	// spt에 존재하지 않는지 확인?
-	struct hash_elem *e = hash_find(&spt->spt_hash, &page->hash_elem);
-	if(e != NULL)
-	{
-		return succ;
-	}
-	//page가 spt에 없으면 삽입
-	hash_insert(&spt->spt_hash, &page->hash_elem);
-	return succ = true;
+	return hash_insert(&spt->spt_hash, &page->hash_elem) == NULL ? true : false; // 존재하지 않을 경우에만 삽입
 }
 
 bool delete_page (struct hash *pages, struct page *p)
