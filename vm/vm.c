f0b97a013dddaf57c49890f2a06ca92d1b466d05
inhokang <ubuntu@ip-172-31-42-48.ap-northeast-2.compute.internal>
2023-06-21 15:52:39 +0000

    supplemental_page_table_init,page_hash,page_less

--- vm/vm.c
+++ vm/vm.c
@@ -173,8 +173,29 @@ vm_do_claim_page (struct page *page) {
 
 /* Initialize new supplemental page table */
 void
-supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) 
+{
+	hash_init(&spt->spt_hash, page_hash, page_less, NULL);
 }
+
+/* Returns a hash value for page p. */
+unsigned
+page_hash(const struct hash_elem *p_, void *aux UNUSED)
+{
+	const struct page *p = hash_entry(p_, struct page, hash_elem);
+	return hash_bytes(&p->va, sizeof p->va);
+}
+
+/* Returns true if page a precedes page b. */
+bool page_less(const struct hash_elem *a_,
+			   const struct hash_elem *b_, void *aux UNUSED)
+{
+	const struct page *a = hash_entry(a_, struct page, hash_elem);
+	const struct page *b = hash_entry(b_, struct page, hash_elem);
+
+	return a->va < b->va;
+}
+
 
 /* Copy supplemental page table from src to dst */
 bool
