4b769dea3e1e43d884e5125cd339f5765700df84
inhokang <ubuntu@ip-172-31-42-48.ap-northeast-2.compute.internal>
2023-06-23 13:34:28 +0000

    spt_remove_page,file_backed_destroy수정

--- vm/vm.c
+++ vm/vm.c
@@ -109,6 +109,7 @@ bool spt_insert_page(struct supplemental_page_table *spt UNUSED,
 
 void
 spt_remove_page (struct supplemental_page_table *spt, struct page *page) {
+	hash_delete(&thread_current()->spt.spt_hash, &page->hash_elem);
 	vm_dealloc_page (page);
 	return true;
 }
